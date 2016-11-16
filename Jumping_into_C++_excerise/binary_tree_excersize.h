9fb722af7a42273828bdc7d5af1d28222762facd
SnowGushiGit <snow.akogi.pgel@gmail.com>
2016-11-17 08:38:20 +0900

    update study

---
+++ Jumping_into_C++_excerise/binary_tree_excersize.h
@@ -0,0 +1,17 @@
+/*
+ * binary_tree_excersize.h
+ *
+ *  Created on: Nov 13, 2016
+ *      Author: masayaogushi
+ */
+
+#ifndef BINARY_TREE_EXCERSIZE_H_
+#define BINARY_TREE_EXCERSIZE_H_
+
+class binary_tree_excersize {
+public:
+	binary_tree_excersize();
+	virtual ~binary_tree_excersize();
+};
+
+#endif /* BINARY_TREE_EXCERSIZE_H_ */
